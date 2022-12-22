2e983c666eb935880fe40b504ffc746dd27d4f3e
mnaofficial <mna.official92@gmail.com>
2022-12-22 21:37:20 +0600

    Home page setup

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import Banner from '../Components/Home/Banner';
+import BusinessSummary from '../Components/Home/BusinessSummary';
+import ContactUs from '../Components/Home/ContactUs';
+import Frequescy from '../Components/Home/Frequescy';
+import GoogleLocationMap from '../Components/Home/GoogleLocationMap';
+import InfoFooter from '../Components/Home/InfoFooter';
+import OfferDateline from '../Components/Home/OfferDateline';
+import OurStock from '../Components/Home/OurStock';
+import Products from '../Components/Home/Products';
+import Review from '../Components/Home/Review';
+import Subscribe from '../Components/Home/Subscribe';
+import WhyChooseUs from '../Components/Home/WhyChooseUs';
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className='mt-8 pt-8'>
+            <Banner />
+            <Products />
+            <BusinessSummary />
+            <Review />
+            <WhyChooseUs />
+            <OfferDateline />
+            <Frequescy />
+            <OurStock />
+            <ContactUs />
+            <Subscribe />
+            <GoogleLocationMap />
+            <InfoFooter />
         </div>
     );
 };
