3dd25c2bec3f8a3052593e3dd24ce4e0ad219dd9
mnaofficial <mna.official92@gmail.com>
2022-12-22 21:33:18 +0600

    blogs page setup

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Blog from '../Components/Blogs/Blog';
 
 const Blogs = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Blog />
+        </>
     );
 };
 
