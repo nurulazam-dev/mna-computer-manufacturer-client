d0d59051b68d5795ab8f30302a87bfc34d91594a
mnaofficial <mna.official92@gmail.com>
2022-12-22 22:33:43 +0600

    useToken hook setup

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
-const useToken = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default useToken;
+const useToken = (user) => {
+    const [token, setToken] = useState("");
+  
+    useEffect(() => {
+      const email = user?.user?.email;
+      const currentUser = { email: email };
+  
+      if (email) {
+        fetch(`https://mna-computer-manufacturer.onrender.com/users?email=${email}`, {
+          method: "PUT",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(currentUser),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            const accessToken = data.token;
+            setToken(accessToken);
+            localStorage.setItem("accessToken", accessToken);
+          });
+      }
+    }, [user]);
+    return [token];
+  };
+  
+  export default useToken;
+  
