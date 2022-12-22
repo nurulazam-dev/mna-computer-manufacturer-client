ee909fce50812a78aae87ae186715253b497838c
mnaofficial <mna.official92@gmail.com>
2022-12-22 22:31:52 +0600

    useAdmin hooks setup

--- src/hooks/useAdmin.js
+++ src/hooks/useAdmin.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import { useEffect, useState } from "react"
 
-const useAdmin = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default useAdmin;
+const useAdmin = (user) => {
+    const [admin, setAdmin] = useState(false);
+    const [adminLoading, setAdminLoading] = useState(true);
+  
+    useEffect(() => {
+      const email = user?.email;
+  
+      if (email) {
+        fetch(`https://mna-computer-manufacturer.onrender.com/users/admin/${email}`, {
+          method: "GET",
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.admin) {
+              setAdmin(data.admin);
+            } else {
+              setAdmin(false);
+            }
+            setAdminLoading(false);
+          });
+      }
+    }, [user]);
+    return [admin, adminLoading];
+  };
+  
+  export default useAdmin;
+  
