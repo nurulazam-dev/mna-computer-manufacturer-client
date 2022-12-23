46cc227ad757d00e46c06da60bbae2762eb761df
mnaofficial <mna.official92@gmail.com>
2022-12-23 20:13:25 +0600

    Footer component setup

--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
-        <div>
-            
-        </div>
+        <footer className='py-5 bg-black opacity-100'>
+                <div className="flex_block text-white text-center text-[13px] md:text-[14px] lg:text-[15px]">
+                    <p>MNA COMPUTER MANUFACTURER</p>
+                    <p> All Rights Reserved . Copyright © {year}  </p>
+                    <p> Designed by @mnaofficialbd </p>
+                </div>
+        </footer>
     );
 };
 
