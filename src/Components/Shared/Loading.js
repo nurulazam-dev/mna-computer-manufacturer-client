f500bc8ac3af56ea70c9d9f37aeb09c6e14da4e2
mnaofficial <mna.official92@gmail.com>
2022-12-23 20:09:08 +0600

    Loading component setup

--- src/Components/Shared/Loading.js
+++ src/Components/Shared/Loading.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div>
-            
+        <div className="flex items-center justify-center ">
+            <div className="w-16 h-16 border-b-2 border-green-700 rounded-full animate-spin"></div>
         </div>
     );
 };
