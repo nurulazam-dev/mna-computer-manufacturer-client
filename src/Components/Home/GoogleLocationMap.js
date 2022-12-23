b0a363eae0372c13e4f4977ec49bcac28999209d
mnaofficial <mna.official92@gmail.com>
2022-12-23 21:21:09 +0600

    Google_Location_Map component setup

--- src/Components/Home/GoogleLocationMap.js
+++ src/Components/Home/GoogleLocationMap.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-const GoogleLocationMap = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const containerStyle = {
+    width: '100%',
+    height: '60%'
+}
+
+class GoogleLocationMap extends Component {
+    render() {
+        return (
+            <section className='pb-[460px]'>
+                <div className='text-center mb-6'>
+                    <h2 className='text-4xl mb-2 text-orange-500 font-bold'>Our Location</h2>
+                    <h4 className='text-2xl'>Our location is available on google map</h4>
+                </div>
+                <div className="">
+                    <Map containerStyle={containerStyle} google={this.props.google} zoom={14}>
+
+                        <Marker onClick={this.onMarkerClick}
+                            name={'Current location'} />
+
+                        <InfoWindow onClose={this.onInfoWindowClose}>
+                        </InfoWindow>
+                    </Map>
+                </div>
+            </section>
+        );
+    };
 };
 
-export default GoogleLocationMap;
+export default GoogleApiWrapper({
+    apiKey: ("AIzaSyCT9VxlQ6qaHeGSG7JyBTlVXbtwZ4rkcFM")
+})(GoogleLocationMap)
