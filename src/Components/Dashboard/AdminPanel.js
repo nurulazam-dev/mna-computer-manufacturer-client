e9fb13a550bd37d9e4fb0172bf7ba5389788edc4
mnaofficial <mna.official92@gmail.com>
2022-12-24 12:37:39 +0600

    Admin_panel component setup

--- src/Components/Dashboard/AdminPanel.js
+++ src/Components/Dashboard/AdminPanel.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import { toast } from "react-toastify";
+import useAdmin from "../../hooks/useAdmin";
 
-const AdminPanel = () => {
+const AdminPanel = ({ user, index, refetch }) => {
+    const { _id, email } = user;
+    const [admin] = useAdmin(user);
+
+    const handleAdmin = () => {
+        fetch(`https://mna-computer-manufacturer.onrender.com/users/admin/${_id}`, {
+            method: "PUT",
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+        })
+            .then((res) => {
+                if (res.status === 403) {
+                    toast.error("You are unauthorized to make admin");
+                }
+                return res.json();
+            })
+            .then((data) => {
+                if (data.modifiedCount) {
+                    toast.success("Admin made Success");
+                }
+                refetch();
+            });
+    };
+
     return (
-        <div>
-            
-        </div>
+        <tr className="hover" key={index}>
+            <th>{index + 1}</th>
+            <td>{email}</td>
+            <td>
+                {admin ? (<p className="text-success font-semibold ml-3">Admin</p>) : (
+                    <button onClick={handleAdmin} className="btn btn-sm btn-primary text-white" >
+                        Make admin
+                    </button>
+                )}
+            </td>
+        </tr>
     );
 };
 
