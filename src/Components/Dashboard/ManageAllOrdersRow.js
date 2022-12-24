3ac70b84531511ce3f89a5e1c7d0ea600498a498
mnaofficial <mna.official92@gmail.com>
2022-12-24 12:53:59 +0600

    Manage_All_Orders_Row component setup

--- src/Components/Dashboard/ManageAllOrdersRow.js
+++ src/Components/Dashboard/ManageAllOrdersRow.js
@@ -1,10 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { toast } from "react-toastify";
+import AdminDeleteOrderModal from './AdminDeleteOrderModal';
 
-const ManageAllOrdersRow = () => {
+const ManageAllOrdersRow = ({ order, index, refetch }) => {
+    const [adminOrderDelete, setAdminOrderDelete] = useState(null);
+    const { _id, customer, customerName, productName, paid, shipment } = order || "";
+
+    const handlePending = () => {
+        fetch(`https://mna-computer-manufacturer.onrender.com/order/${_id}`, {
+            method: "PUT",
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                if (data.modifiedCount) {
+                    toast.success("Placed for shipment");
+                }
+                refetch();
+            });
+    };
+
     return (
-        <div>
-            
-        </div>
+            <tr>
+                <th>{index + 1}</th>
+                <td>{customerName}</td>
+                <td className='text-center'>{customer}</td>
+                <td className='text-center'>{productName}</td>
+                <td>
+                    {paid ? (
+                        <>
+                            <span className="text-success font-semibold">Paid</span>
+                        </>
+                    ) : (
+                        <div className="tooltip tooltip-primary" data-tip="Payment is not completed yet" >
+                            <span className="text-primary font-semibold">Unpaid</span>
+                        </div>
+                    )}
+                </td>
+
+                <td>
+                    {paid ? (
+                        <>
+                            {!shipment ? (
+                                <div className="tooltip tooltip-secondary" data-tip="Place for shipment" >
+                                    <button className="btn btn-sm btn-secondary text-white" onClick={handlePending}>
+                                        Pending
+                                    </button>
+                                </div>
+                            ) : (
+                                <>
+                                    <div className="tooltip tooltip-success" data-tip="Shipment placed" >
+                                        <span className="text-success font-semibold">Shipped</span>
+                                    </div>
+                                </>
+                            )}
+                        </>
+                    ) : (
+                        <>
+                            <div className="tooltip tooltip-warning" data-tip="Delete customer's order" >
+                                <label htmlFor="admin-delete-single-order" className="btn btn-sm btn-warning modal-button"
+                                    onClick={() => setAdminOrderDelete(order)} >
+                                    Cancel <FontAwesomeIcon className='' icon={faTrashAlt}></FontAwesomeIcon>
+                                </label>
+                            </div>
+                        </>
+                    )}
+                    {adminOrderDelete && (
+                        <AdminDeleteOrderModal
+                            adminOrderDelete={adminOrderDelete}
+                            refetch={refetch}
+                        ></AdminDeleteOrderModal>
+                    )}
+                </td>
+
+
+            </tr>
     );
 };
 
