4908e60b2b3396181b7699ead080502b7a54b67a
mnaofficial <mna.official92@gmail.com>
2023-01-01 22:23:33 +0600

    Navber problems solve

--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../Firebase/firebase.init';
@@ -8,6 +8,7 @@ import brandLogo from '../../assets/brandLogo.png';
 
 const Navbar = () => {
     const [user, loading] = useAuthState(auth);
+    let [open, setopen] = useState(false);
     const handleSignOut = () => {
         signOut(auth);
         localStorage.removeItem("accessToken");
@@ -15,45 +16,35 @@ const Navbar = () => {
     if (loading) {
         return <Loading />
     }
-    const navLinks = <>
-        <li><NavLink to='/' className='text-xl'>Home</NavLink></li>
-        <li><NavLink to='/products' className='text-xl '>Products</NavLink></li>
-        <li><NavLink to='/dashboard' className='text-xl '>Dashboard</NavLink></li>
-        <li><NavLink to='/myPortfolio' className='text-xl '>Portfolio</NavLink></li>
-        <li><NavLink to='/blogs' className='text-xl '>Blogs</NavLink></li>
-        {!user && <li><Link to={'/register'}>Register</Link></li>}
-        <li>
-            {
-                user ? <button className='btn btn-NavLink btn-outline  text-decoration-none fs-5' onClick={handleSignOut}>Sign out</button> : (<NavLink to='/login' className='rounded-lg text-xl '>Login</NavLink>)
-            }
-        </li>
-    </>
+
+    const liClass = `md:inline-block  md:ml-10 ml-5 md:my-0 my-2`;
+    const navLinkClass = `text-white cursor-pointer font-Barlow font-normal text-sm inline-block my-3  border-b-2 border-transparent hover:border-pink-700 duration-300`;
 
     return (
-        <>
-            <div className="navbar w-full top-0 z-50 lg:px-10 bg-base-100 fixed shadow-xl">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex="0" className="btn btn-circle swap swap-rotate mx-2 lg:hidden">
-                            <input type="checkbox" />
-                            <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg>
-                            <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" /></svg>
-                        </label>
-                        <ul tabIndex="0" className="menu bg-base-100 menu-compact dropdown-content mt-3 p-2 shadow w-52">
-                            {navLinks}
-                        </ul>
-                    </div>
-                    <Link to='/' >
-                        <img src={brandLogo} alt="Brand_Logo" width="200" height="200" className="" />
-                    </Link>
-                </div>
-                <div className="navbar-end hidden lg:flex">
-                    <ul className="menu menu-horizontal bg-base-100 gap-x-2 p-0">
-                        {navLinks}
-                    </ul>
-                </div>
+        <nav className="bg-[#19223a] top-0 py-2 fixed w-full z-50 flex items-center justify-between">
+            <div className="md:hidden fixed right-5 cursor-pointer z-50" style={{ color: 'white' }}
+                onClick={() => setopen(!open)}>
+                {open ? (<svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" /></svg>) : (<svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg>)}
             </div>
-        </>
+            <Link to='/' >
+                <img src={brandLogo} alt="Brand_Logo" width="200" height="200" className="ml-7" />
+            </Link>
+            <ul className={`bg-[#19223a] md:pl-10 pr-28 md:static fixed duration-500 ease-linear top-0 md:h-auto h-screen z-10 ${!open ? 'right-[-100%] ' : 'right-0'}`}>
+
+                <li className={liClass}><NavLink to='/' className={navLinkClass}>Home</NavLink></li>
+                <li className={liClass}><NavLink to='/products' className={navLinkClass}>Products</NavLink></li>
+                <li className={liClass}><NavLink to='/dashboard' className={navLinkClass}>Dashboard</NavLink></li>
+                <li className={liClass}><NavLink to='/myPortfolio' className={navLinkClass}>Portfolio</NavLink></li>
+                <li className={liClass}><NavLink to='/blogs' className={navLinkClass}>Blogs</NavLink></li>
+                {!user && <li className={liClass}><Link to={'/register'} className={navLinkClass}>Register</Link></li>}
+                <li className={liClass}>
+                    {
+                        user ? <button className={navLinkClass} onClick={handleSignOut}>Sign out</button> : (<NavLink to='/login' className={navLinkClass}>Login</NavLink>)
+                    }
+                </li>
+
+            </ul>
+        </nav>
 
     );
 };
