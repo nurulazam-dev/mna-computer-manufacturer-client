4625c7a887cb367c5771a717162db87ee7cd88c0
mnaofficialbd <mna.official92@gmail.com>
2024-04-17 15:13:16 +0600

    my-profile component update

--- src/Components/Dashboard/MyProfile.js
+++ src/Components/Dashboard/MyProfile.js
@@ -61,209 +61,218 @@ const MyProfile = () => {
   };
 
   return (
-    <>
-      <div className="flex justify-center">
-        {/* ===================================
-            My Profile Section/Card
+    <section className="border border-green-600 rounded">
+      {/* ===================================
+                      My profile section
             ==================================== */}
-        <div className="card w-2/5 bg-white shadow-xl">
-          <div className="card-body">
-            <h2 className="text-center text-fuchsia-600 text-4xl font-semibold">
-              My Profile
-            </h2>
-            {user && (
-              <>
-                {photoURL ? (
-                  <div className="avatar card items-center w-full justify-center">
-                    <div className="w-22 rounded mr-2">
-                      <img src={photoURL} alt="User Avatar" />
-                    </div>
-                    <h3>{displayName}</h3>
-                  </div>
-                ) : (
-                  <div class="avatar card items-center w-full justify-center">
-                    <div className="w-22 rounded ring ring-primary ring-offset-base-100 ring-offset-2 m-2">
-                      <FontAwesomeIcon
-                        className="pt-2 mx-2"
-                        icon={faUser}
-                        size="4x"
-                      ></FontAwesomeIcon>
-                    </div>
-                    <h3 className="text-2xl font-semibold">{displayName}</h3>
-                  </div>
-                )}
-              </>
+      <div className="bg-green-600">
+        <h2 className="text-white text-center p-1 text-2xl font-semibold">
+          My Profile
+        </h2>
+      </div>
+      <div className="lg:flex px-3">
+        {user && (
+          <div className="lg:w-[350px] pt-1">
+            {photoURL ? (
+              <div className="avatar card items-center w-full justify-center">
+                <div className="w-40 rounded mr-2">
+                  <img src={photoURL} alt="User Avatar" />
+                </div>
+                <h3>{displayName}</h3>
+              </div>
+            ) : (
+              <div class="avatar card items-center w-full justify-center">
+                <div className="w-36 rounded ring ring-primary ring-offset-base-100 ring-offset-2 m-2">
+                  <FontAwesomeIcon
+                    className="pt-2 mx-26 text-[8em]"
+                    icon={faUser}
+                  ></FontAwesomeIcon>
+                </div>
+                <h3 className="text-xl font-semibold mt-2">{displayName}</h3>
+              </div>
             )}
+          </div>
+        )}
+        {/* form fields part */}
+        <div className="w-full px-2">
+          {/* 1st line */}
+          <div className="lg:flex md:flex justify-evenly gap-3">
+            <div className="form-control w-full my-3">
+              <label className="flex w-full border items-center py-2 rounded">
+                <span className="px-2 border-r-2 text-[18px]">Email</span>
+                <input
+                  type="email"
+                  value={email}
+                  name="email"
+                  className="bg-white px-4 text-[16px] input-bordered w-full"
+                  disabled
+                />
+              </label>
+            </div>
+            {/* contact field */}
+            <div className="form-control w-full my-3">
+              <label className="flex w-full border items-center py-2 rounded">
+                <span className="px-2 border-r-2 text-[18px]">Contact</span>
+                <input
+                  type="number"
+                  value={contact ? contact : "Not Updated"}
+                  className="bg-white px-4 text-[16px] input-bordered w-full"
+                  disabled
+                />
+              </label>
+            </div>
+          </div>
+          {/* 2nd line */}
+          <div className="lg:flex md:flex justify-evenly gap-3">
+            {/* education field */}
+            <div className="form-control w-full my-3">
+              <label className="flex w-full border items-center py-2 rounded">
+                <span className="px-2 border-r-2 text-[18px]">Education</span>
+                <input
+                  type="text"
+                  value={education ? education : "Not Updated"}
+                  className="bg-white px-4 text-[16px] input-bordered w-full"
+                  disabled
+                />
+              </label>
+            </div>
+            {/* location field */}
+            <div className="form-control w-full my-3">
+              <label className="flex w-full border items-center py-2 rounded">
+                <span className="px-2 border-r-2 text-[18px]">Address</span>
+                <input
+                  type="text"
+                  value={address ? address : "Not Updated"}
+                  className="bg-white px-4 text-[16px] input-bordered w-full"
+                  disabled
+                />
+              </label>
+            </div>
+          </div>
+          {/* 3rd line */}
+          <div className="lg:flex md:flex justify-evenly gap-3">
+            {/* Portfolio field */}
+            <div className="form-control w-full my-3">
+              <label className="flex w-full border items-center py-2 rounded">
+                <span className="px-2 border-r-2 text-[18px]">Portfolio</span>
+                <input
+                  type="text"
+                  value={portfolio ? portfolio : "Not Updated"}
+                  className="bg-white px-4 text-[16px] input-bordered w-full"
+                  disabled
+                />
+              </label>
+            </div>
 
-            <div>
-              <div className="form-control w-full my-3">
-                <label className="input-group">
-                  <span>Email</span>
-                  <input
-                    type="email"
-                    value={email}
-                    name="email"
-                    className="input text-xl input-bordered w-full"
-                    disabled
-                  />
-                </label>
-              </div>
-              {/* contact field */}
-              <div className="form-control w-full my-3">
-                <label className="input-group">
-                  <span>Contact</span>
-                  <input
-                    type="number"
-                    value={contact ? contact : "Your contact wasn't updated"}
-                    className="input text-xl input-bordered w-full"
-                    disabled
-                  />
-                </label>
-              </div>
-              {/* education field */}
-              <div className="form-control w-full my-3">
-                <label className="input-group">
-                  <span>Education</span>
-                  <input
-                    type="text"
-                    value={
-                      education ? education : "Your education wasn't updated"
-                    }
-                    className="input text-xl input-bordered w-full"
-                    disabled
-                  />
-                </label>
-              </div>
-              {/* location field */}
-              <div className="form-control w-full my-3">
-                <label className="input-group">
-                  <span>Address</span>
-                  <input
-                    type="text"
-                    value={address ? address : "Your address wasn't updated"}
-                    className="input text-xl input-bordered w-full "
-                    disabled
-                  />
-                </label>
-              </div>
-              {/* Portfolio field */}
-              <div className="form-control w-full my-3">
-                <label className="input-group">
-                  <span>Portfolio</span>
-                  <input
-                    type="text"
-                    value={
-                      portfolio
-                        ? portfolio
-                        : "Your portfolio link wasn't updated"
-                    }
-                    className="input text-xl input-bordered w-full "
-                    disabled
-                  />
-                </label>
-              </div>
-
-              {/* linkedIn field */}
-              <div className="form-control w-full my-3">
-                <label className="input-group">
-                  <span>LinkedIn</span>
-                  <input
-                    type="text"
-                    value={
-                      linkedIn ? linkedIn : "Your linkedIn link wasn't updated"
-                    }
-                    className="input text-xl input-bordered w-full "
-                    disabled
-                  />
-                </label>
-              </div>
+            {/* linkedIn field */}
+            <div className="form-control w-full my-3">
+              <label className="flex w-full border items-center py-2 rounded">
+                <span className="px-2 border-r-2 text-[18px]">LinkedIn</span>
+                <input
+                  type="text"
+                  value={linkedIn ? linkedIn : "Not Updated"}
+                  className="bg-white px-4 text-[16px] input-bordered w-full"
+                  disabled
+                />
+              </label>
             </div>
           </div>
         </div>
-
-        {/* ===================================
-            Update profile section/card
+      </div>
+      {/* ===================================
+                      Update profile section
             ==================================== */}
-        <div className="card w-2/6 bg-white shadow-xl mx-7">
-          <div className="card-body">
-            <h2 className="text-center text-orange-500 text-4xl font-semibold">
-              Profile Update
-            </h2>
-            <form onSubmit={handleSubmit}>
-              <div className="form-control w-full  my-2">
-                <label className="label">
-                  <span className="label-text text-xl">Contact</span>
+      <div className="shadow-xl">
+        <div className="bg-green-600">
+          <h2 className="text-white text-center p-1 text-2xl font-semibold">
+            Profile Update
+          </h2>
+        </div>
+        <div className="p-3 pt-2">
+          <form onSubmit={handleSubmit}>
+            {/* 1st line */}
+            <div className="lg:flex md:flex justify-evenly gap-3 mb-3">
+              {/* Contact field */}
+              <div className="form-control w-full">
+                <label className="label p-0">
+                  <span className="text-[18px] mb-[2px]">Contact</span>
                 </label>
                 <input
                   type="number"
                   name="contact"
                   placeholder="Provide your Contact Number"
-                  className="input text-xl input-bordered w-full "
+                  className="bg-white input text-[16px] border border-black w-full "
                   defaultValue={contact}
                 />
               </div>
               {/* education field */}
-              <div className="form-control w-full my-2">
-                <label className="label">
-                  <span className="label-text text-xl">Education</span>
+              <div className="form-control w-full">
+                <label className="label p-0">
+                  <span className="text-[18px] mb-[2px]">Education</span>
                 </label>
                 <input
                   type="text"
                   placeholder="Educational Qualification"
                   name="education"
-                  className="input text-xl input-bordered w-full"
+                  className="bg-white input text-[16px] border border-black w-full"
                   defaultValue={education}
                 />
               </div>
               {/* location field */}
-              <div className="form-control w-full my-2">
-                <label className="label">
-                  <span className="label-text text-xl">Address</span>
+              <div className="form-control w-full">
+                <label className="label p-0">
+                  <span className="text-[18px] mb-[2px]">Address</span>
                 </label>
                 <input
                   type="text"
                   name="address"
                   placeholder="Your Address"
-                  className="input text-xl input-bordered w-full"
+                  className="bg-white input text-[16px] border border-black w-full"
                   defaultValue={address}
                 />
               </div>
-              {/* Portfolio field */}
-              <div className="form-control w-full my-2">
-                <label className="label">
-                  <span className="label-text text-xl">Portfolio</span>
-                </label>
-                <input
-                  type="text"
-                  name="portfolio"
-                  placeholder="Portfolio Website Link"
-                  className="input text-xl input-bordered w-full"
-                  defaultValue={portfolio}
-                />
+            </div>
+            {/* 2nd line */}
+            <div className="lg:flex md:flex justify-between items-end gap-3">
+              <div className="lg:w-2/3 lg:flex md:flex gap-3">
+                {/* Portfolio field */}
+                <div className="form-control w-full">
+                  <label className="label p-0">
+                    <span className="text-[18px] mb-[2px]">Portfolio</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="portfolio"
+                    placeholder="Portfolio Website Link"
+                    className="bg-white input text-[16px] border border-black w-full"
+                    defaultValue={portfolio}
+                  />
+                </div>
+                {/* linkedIn field */}
+                <div className="form-control w-full">
+                  <label className="label p-0">
+                    <span className="text-[18px] mb-[2px]">LinkedIn</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="linkedIn"
+                    placeholder="LinkedIn Account Link"
+                    className="bg-white input text-[16px] border border-black w-full"
+                    defaultValue={linkedIn}
+                  />
+                </div>
               </div>
-              {/* linkedIn field */}
-              <div className="form-control w-full my-2">
-                <label className="label">
-                  <span className="label-text text-xl">LinkedIn</span>
-                </label>
-                <input
-                  type="text"
-                  name="linkedIn"
-                  placeholder="LinkedIn Account Link"
-                  className="input text-xl input-bordered w-full"
-                  defaultValue={linkedIn}
-                />
-              </div>
+
               <input
                 type="submit"
-                className="btn my-2 text-white btn-active w-full"
+                className="btn bg-blue-600 text-white border-blue-900 w-full lg:w-[200px] hover:text-orange-500 lg:mt-0 mt-2"
                 value="Update"
               />
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
       </div>
-    </>
+    </section>
   );
 };
 
