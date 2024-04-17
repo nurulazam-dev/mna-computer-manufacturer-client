2c43483d1a1de5f6430adb71df0ac39fd5c33f5d
mnaofficialbd <mna.official92@gmail.com>
2024-04-17 21:37:04 +0600

    manage-all-orders component update

--- src/Components/Dashboard/ManageAllOrders.js
+++ src/Components/Dashboard/ManageAllOrders.js
@@ -1,52 +1,62 @@
-import React from 'react';
-import { useQuery } from 'react-query';
-import Loading from '../Shared/Loading';
-import ManageAllOrdersRow from './ManageAllOrdersRow';
+import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading";
+import ManageAllOrdersRow from "./ManageAllOrdersRow";
 
 const ManageAllOrders = () => {
-    const { data: orders, isLoading, refetch } = useQuery("orders", () =>
-        fetch("https://mna-computer-manufacturer.onrender.com/orders", {
-            headers: {
-                authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-            },
-        }).then((res) => res.json())
-    );
+  const {
+    data: orders,
+    isLoading,
+    refetch,
+  } = useQuery("orders", () =>
+    fetch("https://mna-computer-manufacturer.onrender.com/orders", {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
 
-    if (isLoading) {
-        return <Loading></Loading>;
-    }
-    return (
-        <section>
-            <h2 className="text-center text-green-500 text-4xl font-semibold">Manage Orders </h2>
-            <h2 className="text-center text-xl mt-3">Total Orders: {orders?.length} </h2>
-            <div className="overflow-x-auto p-4">
-                <table className="table w-full">
-                    <thead>
-                        <tr className='text-center'>
-                            <th className='text-base'>S. No</th>
-                            <th className='text-base'>Customer Name</th>
-                            <th className='text-base'>Customer</th>
-                            <th className='text-base'>Product / Part</th>
-                            <th className='text-base'>Status</th>
-                            <th className='text-base'>Update Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            orders?.map((order, index) => <>
-                                <ManageAllOrdersRow
-                                key={order._id}
-                                index={index}
-                                order={order}
-                                refetch={refetch}
-                                ></ManageAllOrdersRow>
-                            </>)
-                        }
-                    </tbody>
-                </table>
-            </div>
-        </section>
-    );
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+  return (
+    <section className="border border-green-600 rounded mx-4">
+      <div className="bg-green-600">
+        <h2 className="text-white text-center p-1 text-2xl font-semibold">
+          Manage Orders
+        </h2>
+      </div>
+      <h2 className="text-center text-xl mt-3">
+        Total Orders: {orders?.length}{" "}
+      </h2>
+      <div className="overflow-x-auto p-4">
+        <table className="table w-full border rounded">
+          <thead>
+            <tr className="text-[17px] text-white">
+              <th className="">S. No</th>
+              <th className="">Customer Name</th>
+              <th className="">Customer</th>
+              <th className="">Product / Part</th>
+              <th className="">Status</th>
+              <th className="">Update Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders?.map((order, index) => (
+              <>
+                <ManageAllOrdersRow
+                  key={order._id}
+                  index={index}
+                  order={order}
+                  refetch={refetch}
+                ></ManageAllOrdersRow>
+              </>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
 };
 
 export default ManageAllOrders;
--- src/Components/Dashboard/ManageAllOrdersRow.js
+++ src/Components/Dashboard/ManageAllOrdersRow.js
@@ -1,86 +1,107 @@
-import React, { useState } from 'react';
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 import { toast } from "react-toastify";
-import AdminDeleteOrderModal from './AdminDeleteOrderModal';
+import AdminDeleteOrderModal from "./AdminDeleteOrderModal";
 
 const ManageAllOrdersRow = ({ order, index, refetch }) => {
-    const [adminOrderDelete, setAdminOrderDelete] = useState(null);
-    const { _id, customer, customerName, productName, paid, shipment } = order || "";
+  const [adminOrderDelete, setAdminOrderDelete] = useState(null);
+  const { _id, customer, customerName, productName, paid, shipment } =
+    order || "";
 
-    const handlePending = () => {
-        fetch(`https://mna-computer-manufacturer.onrender.com/order/${_id}`, {
-            method: "PUT",
-            headers: {
-                authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-            },
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-                if (data?.modifiedCount) {
-                    toast.success("Placed for shipment");
-                }
-                refetch();
-            });
-    };
+  const handlePending = () => {
+    fetch(`https://mna-computer-manufacturer.onrender.com/order/${_id}`, {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data?.modifiedCount) {
+          toast.success("Placed for shipment");
+        }
+        refetch();
+      });
+  };
 
-    return (
-            <tr>
-                <th>{index + 1}</th>
-                <td>{customerName}</td>
-                <td className='text-center'>{customer}</td>
-                <td className='text-center'>{productName}</td>
-                <td>
-                    {paid ? (
-                        <>
-                            <span className="text-success font-semibold">Paid</span>
-                        </>
-                    ) : (
-                        <div className="tooltip tooltip-primary" data-tip="Payment is not completed yet" >
-                            <span className="text-primary font-semibold">Unpaid</span>
-                        </div>
-                    )}
-                </td>
+  return (
+    <tr className="text-[16px]">
+      <th className="bg-white">{index + 1}</th>
+      <td className="bg-white">{customerName}</td>
+      <td className="bg-white">{customer}</td>
+      <td className="bg-white">{productName}</td>
+      <td className="bg-white">
+        {paid ? (
+          <>
+            <span className="text-success font-semibold">Paid</span>
+          </>
+        ) : (
+          <div
+            className="tooltip tooltip-primary"
+            data-tip="Payment is not completed yet"
+          >
+            <span className="text-primary font-semibold">Unpaid</span>
+          </div>
+        )}
+      </td>
 
-                <td>
-                    {paid ? (
-                        <>
-                            {!shipment ? (
-                                <div className="tooltip tooltip-secondary" data-tip="Place for shipment" >
-                                    <button className="btn btn-sm btn-secondary text-white" onClick={handlePending}>
-                                        Pending
-                                    </button>
-                                </div>
-                            ) : (
-                                <>
-                                    <div className="tooltip tooltip-success" data-tip="Shipment placed" >
-                                        <span className="text-success font-semibold">Shipped</span>
-                                    </div>
-                                </>
-                            )}
-                        </>
-                    ) : (
-                        <>
-                            <div className="tooltip tooltip-warning" data-tip="Delete customer's order" >
-                                <label htmlFor="admin-delete-single-order" className="btn btn-sm btn-warning modal-button"
-                                    onClick={() => setAdminOrderDelete(order)} >
-                                    Cancel <FontAwesomeIcon className='' icon={faTrashAlt}></FontAwesomeIcon>
-                                </label>
-                            </div>
-                        </>
-                    )}
-                    {adminOrderDelete && (
-                        <AdminDeleteOrderModal
-                            adminOrderDelete={adminOrderDelete}
-                            refetch={refetch}
-                        ></AdminDeleteOrderModal>
-                    )}
-                </td>
-
-
-            </tr>
-    );
+      <td className="bg-white">
+        {paid ? (
+          <>
+            {!shipment ? (
+              <div
+                className="tooltip tooltip-secondary"
+                data-tip="Place for shipment"
+              >
+                <button
+                  className="btn btn-sm btn-secondary text-white"
+                  onClick={handlePending}
+                >
+                  Pending
+                </button>
+              </div>
+            ) : (
+              <>
+                <div
+                  className="tooltip tooltip-success"
+                  data-tip="Shipment placed"
+                >
+                  <span className="text-success font-semibold">Shipped</span>
+                </div>
+              </>
+            )}
+          </>
+        ) : (
+          <>
+            <div
+              className="tooltip tooltip-warning"
+              data-tip="Delete customer's order"
+            >
+              <label
+                htmlFor="admin-delete-single-order"
+                className="btn btn-sm btn-warning modal-button"
+                onClick={() => setAdminOrderDelete(order)}
+              >
+                Cancel{" "}
+                <FontAwesomeIcon
+                  className=""
+                  icon={faTrashAlt}
+                ></FontAwesomeIcon>
+              </label>
+            </div>
+          </>
+        )}
+        {adminOrderDelete && (
+          <AdminDeleteOrderModal
+            adminOrderDelete={adminOrderDelete}
+            refetch={refetch}
+          ></AdminDeleteOrderModal>
+        )}
+      </td>
+    </tr>
+  );
 };
 
 export default ManageAllOrdersRow;
